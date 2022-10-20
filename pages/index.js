24fd5e6cd927c33f413f6be1713d81f8418c7bd3
unknown <hassanad94@gmail.com>
2022-10-20 21:25:19 +0200

    hero adjust

--- pages/index.js
+++ pages/index.js
@@ -123,7 +123,7 @@ export default function Home() {
             </main>
           </div>
         </div>
-        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <div className="lg:absolute lg:inset-y-0 h-72 md:h-[500px] lg:h-[520px] lg:right-0 lg:w-1/2">
           <Image
             layout="fill"
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
