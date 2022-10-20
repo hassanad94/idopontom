170356a45fa9f9f934bae17e9d4972952f5eca99
unknown <hassanad94@gmail.com>
2022-10-20 21:02:51 +0200

    color changes

--- pages/index.js
+++ pages/index.js
@@ -91,7 +91,7 @@ export default function Home() {
                   <span className="block xl:inline">
                     Ídőpont foglaló alkalmazás
                   </span>{" "}
-                  <span className="block text-indigo-600 xl:inline">
+                  <span className="block text-blue-600 xl:inline">
                     Vállalkozások Számára
                   </span>
                 </h1>
@@ -104,7 +104,7 @@ export default function Home() {
                   <div className="rounded-md shadow">
                     <a
                       href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                     >
                       Alkalmazás Letöltése
                     </a>
@@ -112,7 +112,7 @@ export default function Home() {
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <a
                       href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
                     >
                       Regisztráció
                     </a>
@@ -134,7 +134,7 @@ export default function Home() {
       <div className="py-12 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="lg:text-center">
-            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase">
               Ídőpont Foglaló
             </h2>
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
@@ -151,7 +151,7 @@ export default function Home() {
               {features.map((feature) => (
                 <div key={feature.name} className="relative">
                   <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
                       <feature.icon className="h-6 w-6" aria-hidden="true" />
                     </div>
                     <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
@@ -172,7 +172,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Készen állsz?</span>
-            <span className="block text-indigo-600">Vágj bele még ma!</span>
+            <span className="block text-blue-600">Vágj bele még ma!</span>
           </h2>
           <div className="mt-8 flex flex-wrap gap-y-[10px] justify-center lg:mt-0 lg:flex-shrink-0">
             <div className="flex rounded-md">
@@ -190,7 +190,7 @@ export default function Home() {
             <div className="ml-3 inline-flex rounded-md shadow">
               <a
                 href="#"
-                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50"
               >
                 További információk
               </a>
