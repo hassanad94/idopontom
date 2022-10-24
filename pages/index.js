b0651a539409c452865566f1bceef993f5f70d43
unknown <hassanad94@gmail.com>
2022-10-25 00:46:28 +0200

    bg color change

--- pages/index.js
+++ pages/index.js
@@ -105,7 +105,7 @@ export default function Home() {
                   <div className="rounded-md shadow">
                     <a
                       href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                     >
                       Alkalmazás Letöltése
                     </a>
